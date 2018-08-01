8c285aac6637db7750668b880e6b9fcd3b929db2
TIshow <febooooooo@gmail.com>
2018-08-01 14:46:16 +0300

    Add files via upload

---
+++ TextGenerator-master/execute.py
@@ -0,0 +1,12 @@
+from PrepareChain import PrepareChain
+
+
+text = u"適当な長い文章。長い文章。"
+
+chain = PrepareChain(text)
+triplet_freqs = chain.make_triplet_freqs()
+chain.save(triplet_freqs, True)
+
+from GenerateText import GenerateText
+generator = GenerateText()
+print (generator.generate())
